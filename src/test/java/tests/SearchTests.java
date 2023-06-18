a7140fd21d28233b4232ac390b0966681ed5ba29
Denis Litinskiy <plnvoran@mail.ru>
2023-06-18 10:30:29 +0300

    Добавил тэг андройд

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -2,13 +2,14 @@ package tests;
 
 import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static io.qameta.allure.Allure.step;
-
+@Tag("android")
 public class SearchTests extends TestBase {
 
     @Test
