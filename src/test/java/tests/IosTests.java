a0e49cd301653a59703054e36c92e43a0de0b52a
Denis Litinskiy <plnvoran@mail.ru>
2023-06-18 11:08:12 +0300

    ios test fix

--- src/test/java/tests/IosTests.java
+++ src/test/java/tests/IosTests.java
@@ -1,9 +1,11 @@
 package tests;
 
+import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static io.appium.java_client.AppiumBy.accessibilityId;
 import static io.qameta.allure.Allure.step;
@@ -13,21 +15,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class IosTests extends TestBase {
 
     @Test
-    @DisplayName("Ios simple test")
-    void test01() {
-        step("Click Text Button", () -> {
-            $(accessibilityId("Text Button")).click();
-        });
+    @Tag("ios")
+    @DisplayName("Authorization test")
+    void loginTest() {
+        step("Click button", () ->
+                $(AppiumBy.accessibilityId("Text Button")).click());
 
-        step("Click Text Input", () -> {
-            $(accessibilityId("Text Input")).click();
-            $(accessibilityId("Text Input")).sendKeys("hello@browserstack.com");
+        step("Type email", () -> {
+            $(AppiumBy.id("Text Input")).sendKeys("hello@browserstack.com");
+            $(AppiumBy.id("Text Input")).pressEnter();
         });
-
-        step("Click Text Output", () -> {
-            $(accessibilityId("Text Output")).click();
-
-            assertEquals("hello@browserstack.com", $(accessibilityId("Text Input")).getText());
-        });
+        step("Check output", () ->
+                $(AppiumBy.id("Text Output")).shouldHave(text("hello@browserstack.com")));
     }
 }
