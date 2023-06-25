644f10e5aad731d4509b1f1b5451740c93c0f4ef
Denis Litinskiy <plnvoran@mail.ru>
2023-06-25 19:38:08 +0300

    add param

--- src/test/java/config/MobileConfig.java
+++ src/test/java/config/MobileConfig.java
@@ -3,7 +3,7 @@ package config;
 import org.aeonbits.owner.Config;
 
 @Config.Sources({
-        "classpath:${env}.properties"
+        "classpath:${deviceHost}.properties"
 })
 public interface MobileConfig extends Config {
     @Key("bs")
